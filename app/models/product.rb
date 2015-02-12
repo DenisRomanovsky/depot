beedfa3a6fd2958e9e975e01bc13aab3ba18ada2
Niafa <niafasmail@gmail.com>
2015-02-12 17:30:58 +0300

    Validation to products is ready.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true,
+   format: {
+    with: %r{\.(png|jpg|gif|jpeg)\Z}i,
+    message: "is incorrect. Only jpg. or .gif or. png files are allowed."}
 end
