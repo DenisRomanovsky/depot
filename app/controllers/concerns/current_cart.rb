aba5f3ab94800fad3dc4f87d6b50ee400f0e6901
Niafa <niafasmail@gmail.com>
2015-02-14 00:12:45 +0300

    Basic Cart is customized. Basic tests are ready.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -6,10 +6,16 @@ module CurrentCart
 #Session method for Carts.
 
   def set_cart
+
     @cart = Cart.find(session[:cart_id])
-    rescue ActiveRecord:RecordNotFound
-    @cart = Cart.Create
+    rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
 
     session[:cart_id] = @cart.id
+    session[:show_store_counter] = 0
+  end
+
+  def index_counter_increase
+
   end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    session[:show_store_counter] ? session[:show_store_counter] += 1 : session[:show_store_counter] = 1
+
+    @show_store_counter = session[:show_store_counter]
+
   end
 end
